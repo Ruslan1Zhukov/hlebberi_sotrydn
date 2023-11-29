3abd708543ea23ace6b8e919ff812c1acb421588
Ruslan Zhukov <Ruslan.1Zhukov@yandex.ru>
2023-11-29 14:23:25 +0300

    const на рамзмер фото

--- lib/helpers/picker.dart
+++ lib/helpers/picker.dart
@@ -1,13 +1,11 @@
 import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:hlebberi_sotrydn/theme/fil_color.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ColorProject {
-  static const orange = Colors.orange;
-  static const black = Colors.black;
-}
+///Максимальный вес фото в МБ
+const _maxWeight = 5;
 
 Future<void> pickImage() async {
   final ImagePicker picker = ImagePicker();
@@ -46,14 +44,14 @@ Future<void> compressImageIfNeeded(CroppedFile croppedImage) async {
   final File imageFile = File(croppedImage.path);
   final int fileSize = await imageFile.length();
 
-  if (fileSize > (5 * 1024 * 1024)) {
+  if (fileSize > (_maxWeight * 1024 * 1024)) {
     final String targetPath = "${croppedImage.path}_compressed.jpg";
-    final File? compressedImage = (await FlutterImageCompress.compressAndGetFile(
+    final File? compressedImage =
+        (await FlutterImageCompress.compressAndGetFile(
       croppedImage.path,
       targetPath,
       quality: 85,
     )) as File?;
-    if (compressedImage != null) {
-    }
+    if (compressedImage != null) {}
   }
 }
