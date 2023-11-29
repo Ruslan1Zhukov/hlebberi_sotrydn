1d82d4e0f12179ddce3e9178096adc058a78c809
Roman Zhukov <zhukov.roo@gmail.com>
2023-11-29 15:50:34 +0300

    доделана обработка фото (обрезка и сжатие)

--- lib/helpers/picker.dart
+++ lib/helpers/picker.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:hlebberi_sotrydn/theme/fil_color.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
-///Максимальный вес фото в МБ
+/// Максимальный вес фото в МБ
 const _maxWeight = 5;
 
-Future<void> pickImage() async {
+Future<void> pickImage(BuildContext context) async {
   final ImagePicker picker = ImagePicker();
   final XFile? image = await picker.pickImage(source: ImageSource.gallery);
 
@@ -15,9 +16,6 @@ Future<void> pickImage() async {
     final CroppedFile? croppedImage = await ImageCropper().cropImage(
       sourcePath: image.path,
       aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1),
-      compressQuality: 100,
-      maxWidth: 300,
-      maxHeight: 300,
       cropStyle: CropStyle.circle,
       compressFormat: ImageCompressFormat.jpg,
       uiSettings: [
@@ -35,23 +33,77 @@ Future<void> pickImage() async {
     );
 
     if (croppedImage != null) {
-      await compressImageIfNeeded(croppedImage);
+      var compressImageResult = await _compressImage(File(croppedImage.path), 0);
+      if (compressImageResult == null) {
+        debugPrint("Не получилось обработать изображение");
+        return;
+      }
+      // вызваю функцию для загрузки фото на сервер
+      // (для теста показываем экран с фото)
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => _ImageViewTestPage(imageFile: compressImageResult),
+        ),
+      );
+
     }
   }
 }
 
-Future<void> compressImageIfNeeded(CroppedFile croppedImage) async {
-  final File imageFile = File(croppedImage.path);
+Future<File?> _compressImage(File imageFile, int pass) async {
   final int fileSize = await imageFile.length();
+  if (fileSize < (_maxWeight * 1024 * 1024)) return imageFile;
+  final String targetPath = "${imageFile.path}_compressed_$pass.jpg";
+  final XFile? xFile = await FlutterImageCompress.compressAndGetFile(
+    imageFile.path,
+    targetPath,
+    format: CompressFormat.jpeg,
+  );
+  if (xFile == null) return null;
+  final File compressedImage = File(xFile.path);
+  var compressedImageSize = await compressedImage.length();
+  if (compressedImageSize > (_maxWeight * 1024 * 1024)) {
+    return await _compressImage(compressedImage, pass + 1);
+  } else {
+    return compressedImage;
+  }
+}
 
-  if (fileSize > (_maxWeight * 1024 * 1024)) {
-    final String targetPath = "${croppedImage.path}_compressed.jpg";
-    final File? compressedImage =
-        (await FlutterImageCompress.compressAndGetFile(
-      croppedImage.path,
-      targetPath,
-      quality: 85,
-    )) as File?;
-    if (compressedImage != null) {}
+/// Для теста показать картинку
+class _ImageViewTestPage extends StatelessWidget {
+  final File imageFile;
+
+  const _ImageViewTestPage({Key? key, required this.imageFile}) : super(key: key);
+
+  String _getFileSizeInMB(File file) {
+    final int bytes = file.lengthSync();
+    return "${(bytes / (1024 * 1024)).toStringAsFixed(2)} Мб";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String fileSize = _getFileSizeInMB(imageFile);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Просмотр обработанного изображения'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Размер файла: $fileSize',
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Image.file(imageFile),
+          ],
+        ),
+      ),
+    );
   }
 }
