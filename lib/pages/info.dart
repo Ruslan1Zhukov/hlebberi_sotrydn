639e5c58450e790f34d9e2c58da6af2fd4ef4e5b
Ruslan Zhukov <rustam.zhukov.96@mail.ru>
2024-06-06 15:41:54 +0300

    доделал обучалку

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -1,4 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:hlebberi_sotrydn/pages/home.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerWidget extends StatefulWidget {
+  final String videoPath;
+
+  const VideoPlayerWidget({required this.videoPath, super.key});
+
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoPath)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? Container(
+            color: Colors.black,
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              width: 285,
+              height: 600,
+              child: VideoPlayer(_controller),
+            ),
+          )
+        : const SizedBox(
+            width: 285,
+            height: 600,
+            child: Center(child: CircularProgressIndicator()),
+          );
+  }
+}
 
 class InfoPage extends StatelessWidget {
   const InfoPage({super.key});
@@ -19,66 +70,219 @@ class InfoPage extends StatelessWidget {
                     IconButton(
                       icon: const Icon(Icons.close),
                       onPressed: () {
-                        Navigator.of(context).pop();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()),
+                        );
                       },
                     ),
                   ],
                 ),
               ),
-              Expanded(
-                child: Center(
-                  child: Container(
-                    width: 300,
-                    height: 600,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                backgroundImage: AssetImage('assets/avatar.png'),
-                              ),
-                              SizedBox(width: 10),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('Иван Иванов', style: TextStyle(fontWeight: FontWeight.bold)),
-                                  Text('Пекарь'),
-                                ],
-                              ),
-                              Spacer(),
-                            ],
-                          ),
-                        ),
-                        // Ваше содержание экрана, например, график и зарплата
-                        Expanded(
-                          child: Center(
-                            child: Text('Содержимое экрана'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
+              const VideoPlayerWidget(videoPath: 'assets/videos/info1.mov'),
+              const SizedBox(height: 15),
               const Text('Отсканируй QR - отметься на работе'),
               const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () {
-                  // Действие при нажатии на кнопку "Далее"
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const InfoPage2()),
+                  );
                 },
                 style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black, backgroundColor: Colors.yellow,
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.yellow,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+                child: const Text('Далее'),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InfoPage2 extends StatelessWidget {
+  const InfoPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Шаг 2/4', style: TextStyle(fontSize: 16)),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const VideoPlayerWidget(videoPath: 'assets/videos/info2.mov'),
+              const SizedBox(height: 15),
+              const Text('Добавь фото и оставь отзыв в профиле'),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const InfoPage3()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.yellow,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+                child: const Text('Далее'),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InfoPage3 extends StatelessWidget {
+  const InfoPage3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Шаг 3/4', style: TextStyle(fontSize: 16)),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const VideoPlayerWidget(
+                videoPath: 'assets/videos/info3.mov',
+              ),
+              const SizedBox(height: 15),
+              const Text('Узнай свой доход за месяц'),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const InfoPage4()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.yellow,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+                child: const Text('Далее'),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InfoPage4 extends StatelessWidget {
+  const InfoPage4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Шаг 4/4', style: TextStyle(fontSize: 16)),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const VideoPlayerWidget(
+                videoPath: 'assets/videos/info4.mov',
+              ),
+              const SizedBox(height: 15),
+              const Text('Узнай свой доход за смену'),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.yellow,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
                 ),
                 child: const Text('Далее'),
               ),
